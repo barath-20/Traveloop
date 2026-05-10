53f43c3ccc34a4eea450f67c8bc2284b0dedd0bb
Barath Kumar <145177695+barath-20@users.noreply.github.com>
2026-05-10 13:23:18 +0530

    Delete app directory

--- app/api/v1/api.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.endpoints import auth, trips
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(trips.router, prefix="/trips", tags=["trips"])
