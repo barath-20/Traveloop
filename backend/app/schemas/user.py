7ea249f0f95356ef08da52379fe697ea9ca166cf
Barath Kumar <barathkumars81@gmail.com>
2026-05-10 13:50:43 +0530

    fixed UserResponse import error

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -34,6 +34,9 @@ class UserInDB(UserBase):
     class Config:
         from_attributes = True
 
+class UserResponse(UserInDB):
+    pass
+
 class Token(BaseModel):
     access_token: str
     token_type: str
